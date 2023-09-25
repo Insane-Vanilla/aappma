a848247f930b18959eab48ddaa5e648b0f6f73cc
Insane Vanilla <insaane.vanillaa@gmail.com>
2023-09-25 14:56:25 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// horloge
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    m = checkTime(m);
+    document.getElementById('time').innerHTML =
+    "Local Time : "+h + ":" + m;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+   
+  startTime();
+
+
+// Basculer entre l’ajout et la suppression de la classe « responsive » à topnav lorsque l’utilisateur clique sur l’icône
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
